6cfa5216c0d769762d6d998e53ed28443bf765fd
Aswin Gopinathan <40236624+infiniteoverflow@users.noreply.github.com>
2023-01-14 08:38:16 +0530

    Added a notifier

--- lib/utils/notifiers.dart
+++ lib/utils/notifiers.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
+/// Contains Notifiers that does various actions of the Calculator.
 class Notifiers {
+  /// This notifier is used to update the current value on the
+  /// calculator display.
   static final ValueNotifier screenDisplayNotifier = ValueNotifier('');
+
+  /// This notifier is used to show the history of operations done
+  /// by the user.
+  static final ValueNotifier<List<String>> historyDisplayNotifier =
+      ValueNotifier([]);
 }
